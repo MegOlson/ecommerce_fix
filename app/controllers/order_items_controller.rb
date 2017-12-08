cca0253a6aefccf851afdd1a1a139400a97d619e
mo <meganannetteolson@yahoo.com>
2017-12-08 09:02:11 -0800

    Refactor cart view and order item destroy method controller to use ajax when deleting order item.

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -24,7 +24,12 @@ class OrderItemsController < ApplicationController
     @order = current_order
     @item = @order.order_items.find(params[:id])
     @item.destroy
-    @order.save
+    if @order.save
+      respond_to do |format|
+        format.html { redirect_to cart_path }
+        format.js
+      end
+    end
     redirect_to cart_path
   end
 
